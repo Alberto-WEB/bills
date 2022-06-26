08db037d9d037c073c559f609baa74dcbbf5713d
Carlos Guzman <alberto.guzman.220793@gmail.com>
2022-06-26 12:44:06 -0500

    desde registro se manda el campo rol con valor de CLIENT

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('rol')->default(2);
+            $table->string('rol');
             $table->rememberToken();
             $table->timestamps();
         });
