35c13ca4a664f956ea9afb1ec8dc719b42365b23
Carlos Guzman <alberto.guzman.220793@gmail.com>
2022-06-26 16:26:38 -0500

    se crea ruta para realizar compras

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,10 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/producto', [ProductController::class, 'index'])->name('product')->middleware('auth');
+//Select Products for Client
+Route::get('/producto', [ProductController::class, 'index'])->name('client.product')->middleware('auth');
+Route::post('/buy', [ProductController::class, 'buy'])->name('client.buy')->middleware('auth');
+
+//List Products for Admin
 Route::get('/producto-admin', [AdminProductController::class, 'index'])->name('admin.product')->middleware('auth');
 
